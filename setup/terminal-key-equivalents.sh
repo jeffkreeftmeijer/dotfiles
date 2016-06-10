c149894dc405023af19e94d1c1c919b601bad6d8
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-06-10 10:02:02 +0200

    Add Terminal.app keyboard shortcuts screenshot

--- setup/terminal-key-equivalents.sh
+++ setup/terminal-key-equivalents.sh
@@ -6,6 +6,8 @@
 # * ⌃⌘S instead of ⌘S to "Export Text As..."
 # * ⌃⌘W instead of ⌘W to "Close Window"
 # * ⌃⌘Q instead of ⌘Q to "Quit Terminal"
+#
+# ![Setting up keyboard shortcuts for Terminal.app ( › System Preferences… › Keyboard)]
 # 
 # Instead of using System Preferences, you can quickly set these up using
 # `defaults` from the command line:
@@ -15,6 +17,8 @@
 #      "Close Window"      = "@^W";
 #      "Quit Terminal"     = "@^Q";
 #    }'
+#
+# [Setting up keyboard shortcuts for Terminal.app ( › System Preferences… › Keyboard)]: images/terminal-keyboard-shortcuts.png
 
 defaults write -app Terminal NSUserKeyEquivalents '{
   "Export Text As..." = "@^S";
