cf6dafe620e3680db05f1660d90cb2944e36d683
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2016-11-19 22:14:59 +0100

    Move appsignal.terminal to setup.sh

--- bin/config.sh
+++ bin/config.sh
@@ -2,6 +2,3 @@
 defaults write -g InitialKeyRepeat -int 15 # 250ms
 defaults write -g KeyRepeat -int 2 #33ms
 
-# Install appsignal.terminal
-curl -OO https://gist.githubusercontent.com/jeffkreeftmeijer/5a9bb873233b1110eed9e8cd62591a85/raw/appsignal-{light,dark}.terminal
-open appsignal-*.terminal
