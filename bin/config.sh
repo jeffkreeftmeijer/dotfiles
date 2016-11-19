b2f2436ebfbcbcd8a8a98c4596a6b30c29d1f2ce
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2016-11-19 22:21:46 +0100

    Automatically hide the Dock and remove its delay

--- bin/config.sh
+++ bin/config.sh
@@ -4,3 +4,13 @@ defaults write -g KeyRepeat -int 2 #33ms
 
 # Remove all apps from the dock
 defaults write com.apple.dock persistent-apps -array
+
+# Automatically hide the dock
+defaults write com.apple.dock autohide -bool true
+
+# Remove Dock hide delay
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+# Restart the Dock
+killall Dock
