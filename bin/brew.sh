d7aef626dfd14d4f4912284914feb00b7604c76d
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2016-11-21 09:01:26 +0100

    Add tunnelblick to bin/brew.sh

--- bin/brew.sh
+++ bin/brew.sh
@@ -1,2 +1,5 @@
-brew install erlang elixir rbenv tmux neovim/neovim/neovim
-brew cask install slack whatsapp seil wacom-bamboo-tablet
+brew install      erlang elixir rbenv \
+                  tmux neovim/neovim/neovim
+
+brew cask install whatsapp seil wacom-bamboo-tablet \
+                  slack tunnelblick
