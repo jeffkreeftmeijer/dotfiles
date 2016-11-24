a7d92dfb6e1b17d3ad911dacb0854eb12420d109
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2016-11-24 12:16:19 +0100

    Add mongo, redis, and chromedriver to brew.sh

--- bin/brew.sh
+++ bin/brew.sh
@@ -1,8 +1,14 @@
 brew install erlang elixir rbenv \
-             tmux neovim/neovim/neovim ag jpegoptim
+             tmux neovim/neovim/neovim ag jpegoptim postgresql pow \
+	     homebrew/versions/mongodb30 redis chromedriver
+
+# Start Postgres, Redis and Chromedriver
+brew services start postgresql
+brew services start redis
+brew services start chromedriver
+brew services start homebrew/versions/mongodb30
 
 # Pow
-brew install pow
 mkdir -p ~/Library/Application\ Support/Pow/Hosts
 ln -s ~/Library/Application\ Support/Pow/Hosts ~/.pow
 sudo pow --install-system
@@ -10,7 +16,3 @@ pow --install-local
 
 sudo launchctl load -w /Library/LaunchDaemons/cx.pow.firewall.plist
 launchctl load -w ~/Library/LaunchAgents/cx.pow.powd.plist
-
-# Postgres
-brew install postgresql
-brew services start postgresql
